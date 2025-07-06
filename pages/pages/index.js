a1086c74d1a284b30c70c2cc11e4bdc0d7c3dd75
tara-maranda <marandatara@gmail.com>
2025-07-06 14:39:50 -0500

    Create index.js

---
+++ pages/pages/index.js
@@ -0,0 +1,10 @@
+import BrainDump from '../components/BrainDump'
+import Layout from '../components/Layout'
+
+export default function Home() {
+  return (
+    <Layout>
+      <BrainDump />
+    </Layout>
+  )
+}
