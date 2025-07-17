f2658d2d13d494460dd49d7a08f781dd2a4f580a
tara-maranda <marandatara@gmail.com>
2025-07-17 15:35:47 -0500

    Create dashboard.js

---
+++ pages/dashboard.js
@@ -0,0 +1,10 @@
+import MasterDashboard from '../components/MasterDashboard'
+import Layout from '../components/Layout'
+
+export default function Dashboard() {
+  return (
+    <Layout>
+      <MasterDashboard />
+    </Layout>
+  )
+}
